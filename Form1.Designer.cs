a2a551da63f8394ca306069e10215357801e8e48
Dot0exe <dot.exe@outlook.com.tr>
2024-08-08 13:32:40 +0300

    Upadte

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,227 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.txtUrl = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnDownload = new Guna.UI2.WinForms.Guna2Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.chkM4A = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.chkOpus = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.chkFlac = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.chkMp3 = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.guna2Panel1.Controls.Add(this.label1);
+            this.guna2Panel1.Controls.Add(this.guna2ControlBox1);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(619, 25);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dot.exe\'s ClipConverter";
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.ControlBoxStyle = Guna.UI2.WinForms.Enums.ControlBoxStyle.Custom;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Gray;
+            this.guna2ControlBox1.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(574, 0);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 25);
+            this.guna2ControlBox1.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtUrl.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUrl.DefaultText = "";
+            this.txtUrl.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtUrl.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtUrl.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUrl.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUrl.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtUrl.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtUrl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.txtUrl.ForeColor = System.Drawing.Color.Gainsboro;
+            this.txtUrl.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.txtUrl.Location = new System.Drawing.Point(15, 44);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.PasswordChar = '\0';
+            this.txtUrl.PlaceholderForeColor = System.Drawing.Color.Black;
+            this.txtUrl.PlaceholderText = "";
+            this.txtUrl.SelectedText = "";
+            this.txtUrl.Size = new System.Drawing.Size(549, 44);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnDownload.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnDownload.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnDownload.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnDownload.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnDownload.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.btnDownload.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnDownload.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnDownload.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnDownload.Location = new System.Drawing.Point(15, 226);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(180, 45);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblStatus.Location = new System.Drawing.Point(201, 242);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 16);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // chkM4A
+            // 
+            this.chkM4A.AutoSize = true;
+            this.chkM4A.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.chkM4A.CheckedState.BorderRadius = 0;
+            this.chkM4A.CheckedState.BorderThickness = 0;
+            this.chkM4A.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.chkM4A.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.chkM4A.ForeColor = System.Drawing.Color.Gainsboro;
+            this.chkM4A.Location = new System.Drawing.Point(15, 110);
+            this.chkM4A.Name = "chkM4A";
+            this.chkM4A.Size = new System.Drawing.Size(63, 23);
+            this.chkM4A.TabIndex = 4;
+            this.chkM4A.Text = "M4A";
+            this.chkM4A.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.chkM4A.UncheckedState.BorderRadius = 0;
+            this.chkM4A.UncheckedState.BorderThickness = 0;
+            this.chkM4A.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            // 
+            // chkOpus
+            // 
+            this.chkOpus.AutoSize = true;
+            this.chkOpus.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.chkOpus.CheckedState.BorderRadius = 0;
+            this.chkOpus.CheckedState.BorderThickness = 0;
+            this.chkOpus.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.chkOpus.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.chkOpus.ForeColor = System.Drawing.Color.Gainsboro;
+            this.chkOpus.Location = new System.Drawing.Point(15, 133);
+            this.chkOpus.Name = "chkOpus";
+            this.chkOpus.Size = new System.Drawing.Size(68, 23);
+            this.chkOpus.TabIndex = 5;
+            this.chkOpus.Text = "Opus";
+            this.chkOpus.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.chkOpus.UncheckedState.BorderRadius = 0;
+            this.chkOpus.UncheckedState.BorderThickness = 0;
+            this.chkOpus.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            // 
+            // chkFlac
+            // 
+            this.chkFlac.AutoSize = true;
+            this.chkFlac.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.chkFlac.CheckedState.BorderRadius = 0;
+            this.chkFlac.CheckedState.BorderThickness = 0;
+            this.chkFlac.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.chkFlac.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.chkFlac.ForeColor = System.Drawing.Color.Gainsboro;
+            this.chkFlac.Location = new System.Drawing.Point(15, 156);
+            this.chkFlac.Name = "chkFlac";
+            this.chkFlac.Size = new System.Drawing.Size(61, 23);
+            this.chkFlac.TabIndex = 6;
+            this.chkFlac.Text = "Flac";
+            this.chkFlac.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.chkFlac.UncheckedState.BorderRadius = 0;
+            this.chkFlac.UncheckedState.BorderThickness = 0;
+            this.chkFlac.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            // 
+            // chkMp3
+            // 
+            this.chkMp3.AutoSize = true;
+            this.chkMp3.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.chkMp3.CheckedState.BorderRadius = 0;
+            this.chkMp3.CheckedState.BorderThickness = 0;
+            this.chkMp3.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.chkMp3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.chkMp3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.chkMp3.Location = new System.Drawing.Point(15, 179);
+            this.chkMp3.Name = "chkMp3";
+            this.chkMp3.Size = new System.Drawing.Size(62, 23);
+            this.chkMp3.TabIndex = 7;
+            this.chkMp3.Text = "Mp3";
+            this.chkMp3.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.chkMp3.UncheckedState.BorderRadius = 0;
+            this.chkMp3.UncheckedState.BorderThickness = 0;
+            this.chkMp3.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.DragStartTransparencyValue = 1D;
+            this.guna2DragControl1.TargetControl = this.guna2Panel1;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(619, 303);
+            this.Controls.Add(this.chkMp3);
+            this.Controls.Add(this.chkFlac);
+            this.Controls.Add(this.chkOpus);
+            this.Controls.Add(this.chkM4A);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Dot.exe\'s ClipConvert";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox txtUrl;
+        private Guna.UI2.WinForms.Guna2Button btnDownload;
+        private System.Windows.Forms.Label lblStatus;
+        private Guna.UI2.WinForms.Guna2CheckBox chkM4A;
+        private Guna.UI2.WinForms.Guna2CheckBox chkOpus;
+        private Guna.UI2.WinForms.Guna2CheckBox chkFlac;
+        private Guna.UI2.WinForms.Guna2CheckBox chkMp3;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using YoutubeExplode;
 
 namespace Dot.exe_s_ClipConvert
 {
@@ -15,6 +12,145 @@ namespace Dot.exe_s_ClipConvert
         public Form1()
         {
             InitializeComponent();
+            // CheckChanged events for CheckBoxes
+            chkM4A.CheckedChanged += new EventHandler(CheckBox_CheckedChanged);
+            chkOpus.CheckedChanged += new EventHandler(CheckBox_CheckedChanged);
+            chkFlac.CheckedChanged += new EventHandler(CheckBox_CheckedChanged);
+            chkMp3.CheckedChanged += new EventHandler(CheckBox_CheckedChanged);
+        }
+
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            // Ensure only one CheckBox is selected
+            if (sender is CheckBox selectedCheckBox && selectedCheckBox.Checked)
+            {
+                foreach (var control in this.Controls)
+                {
+                    if (control is CheckBox checkBox && checkBox != selectedCheckBox)
+                    {
+                        checkBox.Checked = false;
+                    }
+                }
+            }
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            string videoUrl = txtUrl.Text;
+
+            if (string.IsNullOrWhiteSpace(videoUrl))
+            {
+                MessageBox.Show("Please enter a valid YouTube URL.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            lblStatus.Text = "Downloading...";
+            btnDownload.Enabled = false;
+
+            try
+            {
+                var youtube = new YoutubeClient();
+                var video = await youtube.Videos.GetAsync(videoUrl);
+                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+
+                var streamInfo = streamManifest
+                    .GetAudioOnlyStreams()
+                    .OrderByDescending(s => s.Bitrate)
+                    .FirstOrDefault();
+
+                if (streamInfo != null)
+                {
+                    string musicFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+                    string cleanTitle = CleanFileName(video.Title); // Clean file name
+                    string outputFilePath = Path.Combine(musicFolderPath, $"{cleanTitle}.{streamInfo.Container.Name}");
+                    outputFilePath = GetUniqueFileName(outputFilePath); // Ensure unique file name
+
+                    await youtube.Videos.Streams.DownloadAsync(streamInfo, outputFilePath);
+
+                    // Convert to selected format
+                    if (chkM4A.Checked)
+                    {
+                        ConvertToFormat(outputFilePath, "m4a");
+                    }
+                    if (chkOpus.Checked)
+                    {
+                        ConvertToFormat(outputFilePath, "opus");
+                    }
+                    if (chkFlac.Checked)
+                    {
+                        ConvertToFormat(outputFilePath, "flac");
+                    }
+                    if (chkMp3.Checked)
+                    {
+                        ConvertToFormat(outputFilePath, "mp3");
+                    }
+
+                    lblStatus.Text = "Conversion completed.";
+                }
+                else
+                {
+                    lblStatus.Text = "No audio stream found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lblStatus.Text = "Download failed.";
+            }
+            finally
+            {
+                btnDownload.Enabled = true;
+            }
+        }
+
+        private void ConvertToFormat(string inputFile, string format)
+        {
+            string outputFilePath = Path.ChangeExtension(inputFile, format);
+            outputFilePath = GetUniqueFileName(outputFilePath); // Ensure unique file name
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "ffmpeg.exe",
+                    Arguments = $"-i \"{inputFile}\" \"{outputFilePath}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+
+            if (process.ExitCode == 0)
+            {
+                File.Delete(inputFile);
+            }
+        }
+
+        private string GetUniqueFileName(string filePath)
+        {
+            int count = 1;
+            string fileNameOnly = Path.GetFileNameWithoutExtension(filePath);
+            string extension = Path.GetExtension(filePath);
+            string directory = Path.GetDirectoryName(filePath);
+            string newFilePath = filePath;
+
+            while (File.Exists(newFilePath))
+            {
+                string tempFileName = $"{fileNameOnly}({count++}){extension}";
+                newFilePath = Path.Combine(directory, tempFileName);
+            }
+
+            return newFilePath;
+        }
+
+        private string CleanFileName(string fileName)
+        {
+            foreach (char c in Path.GetInvalidFileNameChars())
+            {
+                fileName = fileName.Replace(c, '_'); // Replace invalid characters with '_'
+            }
+            return fileName;
         }
     }
 }
